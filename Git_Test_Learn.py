67d00c7ef3808afaceeed50019514a64c49de9ae
Prince088 <princebharti8888@gmail.com>
2022-05-26 12:13:57 +0530

    second change

--- Git_Test_Learn.py
+++ Git_Test_Learn.py
@@ -1,1 +1,6 @@
 print('I am testing git and learn how to use it')
+learn = input('did you learned git')
+if learn =='yes' or learn == 'Yes':
+	print('Congratulation')
+else:
+	print('Learn Fast As Soon As possible')
